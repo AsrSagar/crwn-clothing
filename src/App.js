5150917a4c006e6bd23cd3bd2680167cfcf60d83
AsrSagar <sagarray867@gmail.com>
2023-11-04 13:39:12 +0600

    New File

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ import Shop from './components/routes/shop/Shop.component';
 
 import Checkout from './components/routes/checkout/checkout.component';
 
+import ContactUs from './components/contact-us/contact-us.component';
+
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home/>}/>
         <Route path='Shop/*' element={<Shop/>}/>
+        <Route path='contact' element={<ContactUs/>}/>
         <Route path='auth' element={<Authentication/>}/>
         <Route path='Checkout' element={<Checkout/>}/>
       </Route>
---
+++ src/components/contact-us/contact-us.component.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+
+import '../contact-us/contact-us.styles.scss';
+
+const ContactUs = () => {
+    return(
+        <Fragment>
+            <h2>This is contact page</h2>
+        </Fragment>
+    );
+}
+export default ContactUs;
